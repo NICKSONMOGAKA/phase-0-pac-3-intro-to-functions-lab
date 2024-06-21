9e0563c675c3fd3e235b0b566ca5110a4890967d
+ <nickson.mogaka@student.moringaschool.com>
2024-06-21 11:32:13 +0300

    modify

---
+++ index.js
@@ -0,0 +1,36 @@
+const name = "Susan";
+const height = 35;
+const message = `${name} is ${height} inches tall`;
+
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(shout(string));
+}
+
+function logWhisper(string) {
+    console.log(whisper(string));
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+}
+
+module.exports = {
+    name,
+    height,
+    message,
+    shout,
+};
